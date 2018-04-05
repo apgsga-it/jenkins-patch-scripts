d58d85dd91e8e042152e854e6b944c69a515026b
JHE <JHE@LAZHG552433012.affichage-p.ch>
2018-04-05 14:23:13 +0200

    java8mig-144

--- vars/patchfunctions.groovy
+++ vars/patchfunctions.groovy
@@ -266,9 +266,9 @@ def initiateArtifactoryConnection() {
 }
 
 def extractZip(downloadedZip,target) {
+	echo "Looking for following file : ${downloadedZip}"
 	def files = findFiles(glob: '**/${downloadedZip}')
 	def currentDateAndTime = new Date().format('yyyyMMddHHmmss')
 	def extractedFolderName = "java_gui_${currentDateAndTime}"
-	echo "found file path : ${files[0].path}"
 	unzip zipFile: "${files[0].path}", dir: "\\\\gui-${target}.apgsga.ch\\it21_${target}\\getting_extracted_${extractedFolderName}"
 }
