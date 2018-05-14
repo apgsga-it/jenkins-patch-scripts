6669ae0bc7623a6ee3dd9b0d7f758853ab608fcd
JHE <JHE@LAZHG552433012.affichage-p.ch>
2018-05-14 11:23:33 +0200

    Installation script simplified

--- src/main/jenkins/server/patchDbPipeline.groovy
+++ src/main/jenkins/server/patchDbPipeline.groovy
@@ -100,15 +100,8 @@ stage("${target.targetName} Installation") {
 	
 	node (env.JENKINS_INSTALLER){
 		def newFolderName = patchConfig.dbPatchBranch.replace("Patch", "test")
-		// TODO JHE: to be checked if /delete is really needed, but I had cases where I was getting following error: "The local device name is already in use"
-		dir("\\\\cm-linux.apgsga.ch\\cm_patch_download\\${newFolderName}") {
-			bat("net use x: /delete")
-			bat("net use x: \\\\cm-linux.apgsga.ch\\cm_winproc_root /persistent:yes")
-			bat("pushd %~dp0")
-			// TODO JHE: Replace CHEI212 with target ${target.targetName}
-			echo "Forcing simulation on CHEI212, normally it would have been on ${target.targetName}"
-			bat("cmd /c \\\\cm-linux.apgsga.ch\\cm_winproc_root\\it21_extensions\\it21.bat installer patch chei212")
-			bat("popd")
-		}
+		// TODO JHE: Replace CHEI212 with target ${target.targetName}
+		echo "Forcing simulation on CHEI212, normally it would have been on chei212"
+		bat("cmd /c \\\\cm-linux.apgsga.ch\\cm_winproc_root\\it21_extensions\\jenkins_pipeline_patch_install.bat ${newFolderName} chei212")
 	}
 }
