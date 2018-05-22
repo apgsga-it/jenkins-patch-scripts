817b75f836f6697a89e70baa94cc758f2d11d705
Julien Helbling <Julien Helbling@LAZHG552433012.affichage-p.ch>
2018-05-22 16:03:30 +0200

    counter removed

--- src/main/jenkins/server/patchCleaner.groovy
+++ src/main/jenkins/server/patchCleaner.groovy
@@ -23,7 +23,7 @@ node {
 				def lastSuccesffulbuild = job.getLastSuccessfulBuild()
 				if(lastSuccesffulbuild != null) {
 					echo "Job ${jobName} successfully ended and will be moved"
-					nbMovedjob++
+					//nbMovedjob++
 					def NEW_JOB_NAME = "PROD_${jobName}"
 					
 					productivePatchView.doAddJobToView(NEW_JOB_NAME)
