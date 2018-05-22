8d79c0545930e45734e79e23e745d8ad53aa557b
Julien Helbling <Julien Helbling@192.168.68.6>
2018-05-22 20:10:16 +0200

    PatchBuilder and PatchCleaner are now in separated views -> no need to filter them out.

--- src/main/jenkins/server/patchCleaner.groovy
+++ src/main/jenkins/server/patchCleaner.groovy
@@ -12,25 +12,22 @@ node {
 		
 		patchJobs.each { job ->
 			
-			if(!job.name.equalsIgnoreCase("PatchBuilder") && !job.name.equalsIgnoreCase("PatchCleaner")) {
+			def jobName = job.name
 				
-				def jobName = job.name
-				
-				if(!jobName.endsWith("Download")) {
-					def lastSuccesffulbuild = job.getLastSuccessfulBuild()
-					if(lastSuccesffulbuild != null) {
-						echo "Job ${jobName} successfully ended and will be moved"
-						nbMovedjob++
-						def NEW_JOB_NAME = "PROD_" + jobName
-						
-						job.renameTo(NEW_JOB_NAME)
-						
-						productivePatchView.doAddJobToView(NEW_JOB_NAME)
-						
-						patchJobs.each{ jobToBeDeleted -> 
-							if(jobToBeDeleted.name.equalsIgnoreCase(jobName + "Download")) {
-								jobToBeDeleted.delete()
-							}
+			if(!jobName.endsWith("Download")) {
+				def lastSuccesffulbuild = job.getLastSuccessfulBuild()
+				if(lastSuccesffulbuild != null) {
+					echo "Job ${jobName} successfully ended and will be moved"
+					nbMovedjob++
+					def NEW_JOB_NAME = "PROD_" + jobName
+					
+					job.renameTo(NEW_JOB_NAME)
+					
+					productivePatchView.doAddJobToView(NEW_JOB_NAME)
+					
+					patchJobs.each{ jobToBeDeleted -> 
+						if(jobToBeDeleted.name.equalsIgnoreCase(jobName + "Download")) {
+							jobToBeDeleted.delete()
 						}
 					}
 				}
