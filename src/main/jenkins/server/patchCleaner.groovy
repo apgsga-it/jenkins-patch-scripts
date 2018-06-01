d11bb376f2cc82ef28bba2a37f05053a61329110
Julien Helbling <Julien Helbling@192.168.68.18>
2018-06-01 13:16:29 +0200

    JAVA8MIG-180: simple groovy script which only moves jobs.

--- src/main/jenkins/server/patchCleaner.groovy
+++ src/main/jenkins/server/patchCleaner.groovy
@@ -1,74 +1,41 @@
-echo "Patch cleaner starting ..."
-
 def nbMovedjob = 0
 
 def getProductivePatchView() {
 	return hudson.model.Hudson.instance.getView('ProductivePatches')
 }
 
-node {
+def getPatchView() {
+	return hudson.model.Hudson.instance.getView('Patches')
+}
 
-	stage("Moving patch job") {	
-		def JOB_PATTERN = "Patch" //find all jobs starting with "Patch".
-		def patchView = hudson.model.Hudson.instance.getView('Patches')
-		def productivePatchView = getProductivePatchView()
-		def patchJobs = patchView.getItems()
-		
-		patchJobs.each { job ->
+println "Patch cleaner starting ..."
+
+def patchView = getPatchView()
+def productivePatchView = getProductivePatchView()
+def patchJobs = patchView.getItems()
+
+patchJobs.each { job ->
+	def jobName = job.name
+	if(!jobName.endsWith("Download")) {
+		def lastSuccesffulbuild = job.getLastSuccessfulBuild()
+		if(lastSuccesffulbuild != null && !job.isBuilding()) {
+			println "Job ${jobName} successfully ended and will be moved"
+			nbMovedjob++
+			def NEW_JOB_NAME = "PROD_" + jobName
 			
-			def jobName = job.name
-				
-			if(!jobName.endsWith("Download")) {
-				def lastSuccesffulbuild = job.getLastSuccessfulBuild()
-				if(lastSuccesffulbuild != null) {
-					echo "Job ${jobName} successfully ended and will be moved"
-					nbMovedjob++
-					def NEW_JOB_NAME = "PROD_" + jobName
+			job.renameTo(NEW_JOB_NAME)
 					
-					job.renameTo(NEW_JOB_NAME)
+			productivePatchView.doAddJobToView(NEW_JOB_NAME)
 					
-					productivePatchView.doAddJobToView(NEW_JOB_NAME)
-					
-					patchJobs.each{ downloadJob -> 
-						if(downloadJob.name.equalsIgnoreCase(jobName + "Download")) {
-							// JHE(23.05.2018): Not sure if we really want to delete the download job, for now, just move it as well.
-							//jobToBeDeleted.delete()
-							def NEW_DOWNLOADJOB_NAME = "PROD_" + downloadJob.name
-							downloadJob.renameTo(NEW_DOWNLOADJOB_NAME)
-							productivePatchView.doAddJobToView(NEW_DOWNLOADJOB_NAME)
-						}
-					}
+			patchJobs.each{ downloadJob -> 
+				if(downloadJob.name.equalsIgnoreCase(jobName + "Download")) {
+					// JHE(23.05.2018): Not sure if we really want to delete the download job, for now, just move it as well.
+					//jobToBeDeleted.delete()
+					def NEW_DOWNLOADJOB_NAME = "PROD_" + downloadJob.name
+					downloadJob.renameTo(NEW_DOWNLOADJOB_NAME)
+					productivePatchView.doAddJobToView(NEW_DOWNLOADJOB_NAME)
 				}
 			}
 		}
-	}
-	
-	stage("Cleaning Workspaces") {
-		
-		/*
-		 * JHE (23.05.2018): We iterate over all jobs within "ProductivePatches" View. If a Job is there since more than 2 Weeks, then we clean its workspace
-		 */
-		
-		def productivePatchView = getProductivePatchView()
-		def productiveJobs = productivePatchView.getItems()
-		
-		productiveJobs.each { job ->
-			def lastSuccess = job.getLastSuccessfulBuild()
-			
-			// We might have a null for a download job which would never have ran
-			if(lastSuccess != null) {
-				
-				def comparisonDate = new Date()
-				def diff = (lastSuccess.getTime() - comparisonDate).abs()
-				
-				//if(diff >= 1) {
-					def lastSuccessFormated = lastSuccess.getTime().format("YYYY-MMM-dd HH:MM:SS")
-					echo "Last success build for ${job.name} was on ${lastSuccessFormated}, workspace will be clean."
-					job.delete()
-				//}
-			}
-		}
-	}
+	}  
 }
-
-echo "Patch cleaner ended and moved ${nbMovedjob} job(s)"
