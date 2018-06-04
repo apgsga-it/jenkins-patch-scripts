6fe4278757bdbbfb197d54ced3c3a00c5978c9b6
Julien Helbling <Julien Helbling@LAZHG552433012.affichage-p.ch>
2018-06-04 09:46:16 +0200

    .

--- src/main/jenkins/server/patchbuilder.groovy
+++ src/main/jenkins/server/patchbuilder.groovy
@@ -18,6 +18,11 @@ pipelineJob (jobName) {
 	parameters {
 		stringParam('PARAMETER', "", "String mit dem die PatchConfig Parameter als JSON transportiert werden")
 	}
+	listView("Patches") {
+		jobs {
+			names(jobName)
+		}
+	}
 }
 pipelineJob (downLoadJobName) {
 	authenticationToken(downLoadJobName)
@@ -33,9 +38,16 @@ pipelineJob (downLoadJobName) {
 	parameters {
 		stringParam('PARAMETER', "", "String mit dem die PatchConfig Parameter als JSON transportiert werden")
 	}
+	listView("Patches") {
+		jobs {
+			names(downLoadJobName)
+		}
+	}
 }
 
 // JHE (04.06.2018): Ideally, one would use listView section here. The problem with listView is that it adds new job(s), but loose all job which were already listed.
+/*
 def patchView = Hudson.instance.getView("Patches")
 patchView.doAddJobToView(jobName)
 patchView.doAddJobToView(downLoadJobName)
+*/
