72bd973d4a42412d62a5ab8aa87345d1a501b569
Julien Helbling <Julien Helbling@LAZHG552433012.affichage-p.ch>
2018-06-04 07:56:27 +0200

    We don't want to rename Jobs

--- src/main/jenkins/server/patchCleaner.groovy
+++ src/main/jenkins/server/patchCleaner.groovy
@@ -23,19 +23,12 @@ patchJobs.each { job ->
 		if(lastSuccesffulbuild != null && !job.isBuilding()) {
 			println "Job ${jobName} successfully ended and will be moved"
 			nbMovedjob++
-			def NEW_JOB_NAME = "PROD_" + jobName
-			
-			job.renameTo(NEW_JOB_NAME)
-					
-			productivePatchView.doAddJobToView(NEW_JOB_NAME)
-					
+			productivePatchView.doAddJobToView(jobName)
 			patchJobs.each{ downloadJob -> 
 				if(downloadJob.name.equalsIgnoreCase(jobName + "Download")) {
 					// JHE(23.05.2018): Not sure if we really want to delete the download job, for now, just move it as well.
 					//jobToBeDeleted.delete()
-					def NEW_DOWNLOADJOB_NAME = "PROD_" + downloadJob.name
-					downloadJob.renameTo(NEW_DOWNLOADJOB_NAME)
-					productivePatchView.doAddJobToView(NEW_DOWNLOADJOB_NAME)
+					productivePatchView.doAddJobToView(downloadJob.name)
 				}
 			}
 		}
