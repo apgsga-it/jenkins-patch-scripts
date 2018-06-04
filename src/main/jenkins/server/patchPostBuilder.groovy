dc447b2463ecc02275f8cf8932ac5846c40b8d1f
Julien Helbling <Julien Helbling@LAZHG552433012.affichage-p.ch>
2018-06-04 10:11:24 +0200

    java8mig-180: postbuilder which adds job to "Patches" view.

---
+++ src/main/jenkins/server/patchPostBuilder.groovy
@@ -0,0 +1,22 @@
+import hudson.model.*
+import groovy.json.JsonSlurperClassic
+
+properties([
+	parameters([
+		stringParam(
+		defaultValue: "",
+		description: 'Parameter',
+		name: 'PARAMETER'
+		)
+	])
+])
+
+// Parameter
+def jobs = new JsonSlurperClassic().parseText(params.PARAMETER)
+
+println "jobs = ${jobs}"
+
+/*
+def patchView = Hudson.instance.getView('Patches')
+productivePatchView.doAddJobToView(downloadJob.name)
+*/
