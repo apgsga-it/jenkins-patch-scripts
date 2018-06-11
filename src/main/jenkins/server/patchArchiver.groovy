58bf28384579795837c44e48398433247531af39
Julien Helbling <Julien Helbling@LAZHG552433012>
2018-06-11 11:21:15 +0200

    refactored

--- src/main/jenkins/server/patchArchiver.groovy
+++ src/main/jenkins/server/patchArchiver.groovy
@@ -5,6 +5,30 @@ def getProductivePatchView() {
 	return Hudson.instance.getView('ProductivePatches')
 }
 
+def archiveLogsForPipelineJob(def builds) {
+	def buildNr = 1
+	builds.each{ build ->
+		def logContent = build.logFile.text
+		def logName = "${jobName}-archive-${buildNr}.log"
+		new File("/var/opt/apg-jenkins/archive/log/${logName}").write(logContent)
+		buildNr++
+	}
+}
+
+def deleteJenkinsWorkspaces(def jobName) {
+	// Delete Patch workspace folder within /var/jenkins/workspace
+	// JHE (11.06.2018): At best, we would run a command like "rm -rf /var/jenkins/workspace/Patchxxx*" ... but unfortunately we can't use the "*" in the command ... so we have to do some parsing.
+	def jenkinsWorkspaceFolder = "/var/jenkins/workspace"
+	def lsCmd = "ls -la ${jenkinsWorkspaceFolder} | grep '${jobName}'"
+	def lsOutput = ['bash', '-c', lsCmd].execute().in.text
+	lsOutput.eachLine{line ->
+		def folderToDelete = "${jenkinsWorkspaceFolder}/" + line.substring(line.lastIndexOf(" "), line.length()).trim()
+		def rmCmd = "rm -rf ${folderToDelete}"
+		['bash', '-c', rmCmd].execute().in.text
+		println "${folderToDelete} has been deleted!"
+	}
+}
+
 println "Patch archiver starting ..."
 
 def nbProdJobDeleted = 0
@@ -28,23 +52,11 @@ patchJobs.each { job ->
 
 						// Archive all PROD Pipeline logs
 						def prodBuilds = Jenkins.getInstance().getItemByFullName(jobName).getBuilds()
-						def prodBuildNr = 1
-						prodBuilds.each{ prodBuild ->
-							def prodLogContent = prodBuild.logFile.text
-							def prodLogName = "${jobName}-archive-${prodBuildNr}.log"
-							new File("/var/opt/apg-jenkins/archive/log/${prodLogName}").write(prodLogContent)
-							prodBuildNr++
-						}
+						archiveLogsForPipelineJob(prodBuilds)
 
 						// Archive all Download Pipeline logs
 						def downloadBuilds = Jenkins.getInstance().getItemByFullName(downloadJobName).getBuilds()
-						def downloadBuildNr = 1
-						downloadBuilds.each{ downloadBuild ->
-							def downloadLogContent = downloadBuild.logFile.text
-							def downloadLogName = "${downloadJobName}-archive-${downloadBuildNr}.log"
-							new File("/var/opt/apg-jenkins/archive/log/${downloadLogName}").write(downloadLogContent)
-							downloadBuildNr++
-						}
+						archiveLogsForPipelineJob(downloadBuilds)
 
 						job.delete()
 						println "${jobName} has been deleted."
@@ -58,17 +70,7 @@ patchJobs.each { job ->
 		}
 	}
 	
-	// Delete Patch workspace folder within /var/jenkins/workspace
-	// JHE (11.06.2018): At best, we would run a command like "rm -rf /var/jenkins/workspace/Patchxxx*" ... but unfortunately we can't use the "*" in the command ... so we have to do some parsing.
-	def jenkinsWorkspaceFolder = "/var/jenkins/workspace"
-	def lsCmd = "ls -la ${jenkinsWorkspaceFolder} | grep '${jobName}'"
-	def lsOutput = ['bash', '-c', lsCmd].execute().in.text
-	lsOutput.eachLine{line ->
-		def folderToDelete = "${jenkinsWorkspaceFolder}/" + line.substring(line.lastIndexOf(" "), line.length()).trim()
-		def rmCmd = "rm -rf ${folderToDelete}"
-		['bash', '-c', rmCmd].execute().in.text
-		println "${folderToDelete} has been deleted!"
-	}
+	deleteJenkinsWorkspaces(jobName)
 }
 
 println "Patch archiver done."
