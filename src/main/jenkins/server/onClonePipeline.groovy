6cdfad2f7247b8669c920489fc30d4dcc503828d
Julien Helbling <Julien Helbling@LAZHG552433012>
2018-06-27 09:34:59 +0200

    trying to get parameter

--- src/main/jenkins/server/onClonePipeline.groovy
+++ src/main/jenkins/server/onClonePipeline.groovy
@@ -1,7 +1,18 @@
+properties([
+	parameters([
+		stringParam(
+		defaultValue: "",
+		description: 'Parameter',
+		name: 'target'
+		)
+	])
+])
+
 stage("onclone") {
 	
 	stage("resetRevision") {
 		node {
+			def target = params.target
 			echo "Within resetRevision stage ...."
 		}
 	}
