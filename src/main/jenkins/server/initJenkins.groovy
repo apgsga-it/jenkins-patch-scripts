d2b0df7139e23de8c0db37187d1a5c979d0acb0e
Christoph Henrici <christoph.henrici@chesnb.com>
2018-06-28 15:35:53 +0200

    DRY

--- src/main/jenkins/server/initJenkins.groovy
+++ src/main/jenkins/server/initJenkins.groovy
@@ -14,7 +14,7 @@ def resolver = build.buildVariableResolver
  }
 
 def dry = resolver.resolve("DRY")
-print "Running with ${dry}"
+print "Running with DRY:  ${dry}"
 // First Delete Job in Patch Views
 println "Deleting all Job for Patch Views"
 ["ProductivePatches", "Patches"].each { viewName ->
