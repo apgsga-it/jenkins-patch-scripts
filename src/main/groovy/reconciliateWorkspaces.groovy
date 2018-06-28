20850eae65a757d92e3ea6b8dc2292f5c82947c5
Christoph Henrici <chhenrici@gmail.com>
2018-06-28 23:55:24 +0200

    Tests

--- src/main/groovy/reconciliateWorkspaces.groovy
+++ src/main/groovy/reconciliateWorkspaces.groovy
@@ -1,16 +1,23 @@
+#!/usr/bin/env groovy
 def cli = new CliBuilder(usage: '-j|jenkins <directory>')
 cli.with {
+	h longOpt: 'help', 'Show usage information', required: false
 	j longOpt: 'jenkins',args:1 , argName: 'directory', 'Jenkins installation directory', required: true
-	u longOpt: 'update', 'If to run with updates'
+	u longOpt: 'update', 'If to run with updates', required: false
 }
-def options = cli.parse(args)
-if (options == null) {
+def opt = cli.parse(args)
+if (!opt) {
+	System.exit(1)
+}
+if (opt.h)  {
+	cli.usage()
 	System.exit(1)
 }
 // Validate if directory
-def directory = new File(options.j)
+def directory = new File(opt.j)
+print directory
 if (!directory.exists() | !directory.directory) {
-	println "Directory ${options.l} not valid: either not a directory or it doesn't exist"
+	println "Directory ${opt.j} not valid: either not a directory or it doesn't exist"
 	System.exit(1)
 }
 // Validate if Jenkins Installation
@@ -25,8 +32,8 @@ if (!workspacesDir.exists() | !workspacesDir.directory) {
 	System.exit(1)
 }
 // Validate permissions
-def dry = !options.u 
-println "Running with Updates : ${options.u}"
+def dry = !opt.u 
+println "Running with Updates : ${opt.u}"
 if (!dry) {
 	if (!workspacesDir.canWrite()) {
 		println "Not sufficient rights for workspace subdirectory"
@@ -36,11 +43,24 @@ if (!dry) {
 println "Cleaning up workspaces in : ${workspacesDir.getPath()}"
 workspacesDir.eachDir() { dir -> 
 	println "Inspecting workspace Directory: ${dir.getName()}"
-	def jobDir = new File(jobsDir,dir.getName())
+	def pos = dir.getName().indexOf("@")
+	def jobName = pos < 0 ? dir.getName() : dir.getName().substring(0,pos)
+	println "Resolved Jobname : ${jobName}"
+	def jobDir = new File(jobsDir,jobName)
 	if (jobDir.exists()) {
-		println "++++++Corresponding Job Directory exists: ${jobDir.getPath()}"
+		println "++++++ Corresponding Job Directory exists: ${jobDir.getPath()}"
 	} else {
-		println "------No Job for: ${dir.getName()}"
+		println "------ No Job for: ${dir.getName()}"
+		println "Directory ${dir.getName()} can be deleted" 
+		if (!dry) {
+			if (dir.deleteDir()) {
+				println "Directory ${dir.getName()} has been deleted" 
+			} else {
+				println "Directory ${dir.getName()} has NOT been deleted"
+			}
+		} else {
+			println "Running dry ${dir.getName()} not deleted"
+		}
 	}
 }
 
