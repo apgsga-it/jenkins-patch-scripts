35c7ee44d3f9481fbde5b86cc65f051979cef9f8
Christoph Henrici <christoph.henrici@chesnb.com>
2018-07-09 18:23:16 +0200

    pom

--- src/main/groovy/checkForVersion.groovy
+++ src/main/groovy/checkForVersion.groovy
@@ -9,9 +9,10 @@ println "Inspecting all Maven Jobs for Maven Version"
 // Delete the missed Maven Jobs
 jobs = Jenkins.instance.getAllItems(hudson.maven.MavenModuleSet.class).each {  job ->
 	println "About to Inspect Maven Job ${job.name}"
-	def configXMLFile = job.getConfigFile().getFile().getAbsolutePath();
-	println configXMLFile
-	def configXml = new XmlSlurper().parse(configXMLFile)
-	println("Config xml:")
-	println XmlUtil.serialize(configXml).toString()
+	def wrks = new File('/var/jenkins/workspace/${job.name}')
+	if (wrks.exists()) {
+		def pomFile = new File(wrks,"pom.xml")
+		def pomXml = new XmlSlurper().parse(pom)
+		println XmlUtil.serialize(pomXml).toString()
+	}
 }
