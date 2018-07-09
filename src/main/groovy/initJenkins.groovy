c537b61c73b2b246128d06b29475e7775078b8bf
Christoph Henrici <christoph.henrici@chesnb.com>
2018-07-09 16:54:08 +0200

    Dry

--- src/main/groovy/initJenkins.groovy
+++ src/main/groovy/initJenkins.groovy
@@ -21,7 +21,7 @@ println "Deleting all Job for Patch Views"
 	println "Deleteing Jobs from ${viewName}"
 	Jenkins.instance.getView(viewName).items.each { item ->
 		println "About to delete $item.name"
-		if (dry == false) {
+		if (dry.equals('false')) {
 			item.delete()
 			println "Deleted $item.name"
 		} else {
