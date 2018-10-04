0bc765c5d5189454d10251b6d7aaecb432d8da63
Christoph Henrici <christoph.henrici@chesnb.com>
2018-10-04 12:55:54 +0200

    maven and gradle mavenLocal same

--- vars/patchfunctions.groovy
+++ vars/patchfunctions.groovy
@@ -205,7 +205,7 @@ def saveRevisions(patchConfig) {
 
 def isPatchForProdTarget(def patchConfig) {
 	def targetMap = loadTargetsMap()
-	return targetMap.get("Produktion").get("targetName").equalsIgnoreCase("${patchConfig.installationTarget}") 	
+	return targetMap.get("Produktion").get("targetName").equalsIgnoreCase("${patchConfig.installationTarget}")
 }
 
 def saveProdRevision(def patchConfig) {
@@ -308,7 +308,7 @@ def generateVersionProperties(patchConfig) {
 	echo "$buildVersion"
 	dir ("it21-ui-bundle") {
 		sh "chmod +x ./gradlew"
-		sh "./gradlew clean it21-ui-dm-version-manager:publish -PsourceVersion=${previousVersion} -PpublishVersion=${buildVersion} -PpatchFile=file:/${patchConfig.patchFilePath}"
+		sh "./gradlew -Dmaven.repo.local=${patchConfig.mavenLocalRepo} clean it21-ui-dm-version-manager:publish -PsourceVersion=${previousVersion} -PpublishVersion=${buildVersion} -PpatchFile=file:/${patchConfig.patchFilePath}"
 	}
 }
 
@@ -333,17 +333,17 @@ def buildModule(patchConfig,module) {
 def updateBom(patchConfig,module) {
 	echo "Update Bom for artifact " + module.artifactId + " for Revision: " + patchConfig.revision
 	def buildVersion = mavenVersionNumber(patchConfig,patchConfig.revision)
-	
+
 	// TODO JHE: Verify buildVersion for JAVA8MIG-455 .... should probably be "mavenVersionNumber(patchConfig,patchConfig.lastrevision)" .... or apsclirev -lr ????
 	def testLastBuildRevision = mavenVersionNumber(patchConfig,patchConfig.revision) // JHE: Debug variable to be removed
 	def cmd = "/opt/apg-patch-cli/bin/apsrevcli.sh -lr ${patchConfig.installationTarget}" // JHE: Debug  to be removed
 	def lastTargetRevision = sh ( returnStdout : true, script: cmd).trim() // JHE: Debug  to be removed
 	echo "testLastBuildRevision = ${testLastBuildRevision} // buildVersion = ${buildVersion} // lastTargetRevision = ${lastTargetRevision}" // JHE: Debug echo to be removed
 
-	echo "Bom source version which will be update: ${buildVersion}" 
+	echo "Bom source version which will be update: ${buildVersion}"
 	dir ("it21-ui-bundle") {
 		sh "chmod +x ./gradlew"
-		sh "./gradlew clean it21-ui-dm-version-manager:publish -PsourceVersion=${buildVersion} -Partifact=${module.groupId}:${module.artifactId} -PpatchFile=file:/${patchConfig.patchFilePath}"
+		sh "./gradlew -Dmaven.repo.local=${patchConfig.mavenLocalRepo} clean it21-ui-dm-version-manager:publish -PsourceVersion=${buildVersion} -Partifact=${module.groupId}:${module.artifactId} -PpatchFile=file:/${patchConfig.patchFilePath}"
 	}
 }
 
@@ -491,7 +491,7 @@ def assemble(patchConfig, assemblyName) {
 	echo "Building Assembly ${assemblyName} with version: ${buildVersion} "
 	dir ("it21-ui-bundle") {
 		sh "chmod +x ./gradlew"
-		sh "./gradlew ${assemblyName}:assemble ${assemblyName}:publish -PsourceVersion=${buildVersion}"
+		sh "./gradlew -Dmaven.repo.local=${patchConfig.mavenLocalRepo} ${assemblyName}:assemble ${assemblyName}:publish -PsourceVersion=${buildVersion}"
 	}
 }
 
