8c5858518d29a18ea243abc6ca6dbdf1b985c810
Julien Helbling <Julien,Helbling@apgsga.ch>
2019-04-17 10:06:01 +0200

    saving target bean for onDemand

--- src/main/jenkins/server/patchOnDemandPipeline.groovy
+++ src/main/jenkins/server/patchOnDemandPipeline.groovy
@@ -19,7 +19,7 @@ patchfunctions.initPatchConfig(patchConfig,params)
 def defaultNodes = [[label:env.DEFAULT_JADAS_ONDEMAND_NODE,serviceName:"jadas"]]
 def target = [envName:"OnDemand",targetName:patchConfig.installationTarget,nodes:defaultNodes]
 patchConfig.currentTarget = patchConfig.installationTarget
-patchfunctions.saveTarget(patchConfig,target)
+patchConfig.targetBean = target
 patchfunctions.stage(target,"Installationsbereit",patchConfig,"Build", patchfunctions.&patchBuildsConcurrent)
 patchfunctions.stage(target,"Installationsbereit",patchConfig,"Assembly", patchfunctions.&assembleDeploymentArtefacts)
 patchfunctions.stage(target,"Installation",patchConfig,"InstallOldStyle", patchDeployment.&installOldStyle)
