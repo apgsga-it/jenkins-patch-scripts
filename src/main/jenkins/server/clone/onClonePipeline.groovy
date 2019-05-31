bb986e222ac99934919027487ef12363ac369d2e
Julien Helbling <Julien,Helbling@apgsga.ch>
2019-05-31 12:51:42 +0200

    Backport from cm-184

--- src/main/jenkins/server/clone/onClonePipeline.groovy
+++ src/main/jenkins/server/clone/onClonePipeline.groovy
@@ -2,23 +2,8 @@ package clone
 import groovy.json.JsonSlurper
 import groovy.json.JsonSlurperClassic
 
-properties([
-	parameters([
-		stringParam(
-			defaultValue: "",
-			description: 'Parameter',
-			name: 'source'
-			),
-		stringParam(
-			defaultValue: "",
-			description: 'Parameter',
-			name: 'target'
-		)
-	])
-])
-
-def source = params.source
-def target = params.target
+def source = env.source
+def target = env.target
 
 println "Parameter ... source = ${source} , target = ${target}"
 
