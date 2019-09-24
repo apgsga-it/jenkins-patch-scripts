4e802e6c5f8324e1a73f6783a9911964cb0f5c78
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-24 13:18:10 +0200

    forward slashs

--- vars/patchDeployment.groovy
+++ vars/patchDeployment.groovy
@@ -183,7 +183,7 @@ def guiExtractedFolderName() {
 }
 
 def downloadGuiZipToBeInstalled(def groupId, def artifactId, def artifactType, def buildVersion) {
-	def mvnCommand = "mvn dependency:copy -Dartifact=${groupId}:${artifactId}:${buildVersion}:${artifactType} -DoutputDirectory=./download -s C:\\local\\software\\maven\\settings.xml -X"
+	def mvnCommand = "mvn dependency:copy -Dartifact=${groupId}:${artifactId}:${buildVersion}:${artifactType} -DoutputDirectory=./download -s C:/local/software/maven/settings.xml -X"
 	echo "Downloading GUI-ZIP with following command: ${mvnCommand}"
 	withMaven( maven: 'apache-maven-3.5.0') { sh "${mvnCommand}" }
 	echo "GUI-ZIP correctly downloaded."
