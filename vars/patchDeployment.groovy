a38775b7fc533f57bad8425beb34f17ea386acc4
apgsga-jhe <Julien.Helbling@apgsga.ch>
2019-11-04 11:46:30 +0100

    Need chmod 755 on gettingExtracted* folder

--- vars/patchDeployment.groovy
+++ vars/patchDeployment.groovy
@@ -117,6 +117,7 @@ def it21UiInstaller(target,host,buildVersion) {
 			
 			// TODO JHE: Best to run all in one script ? ... not sure
 			ssh(host, "sudo mkdir /etc/opt/it21_ui_${target}/gettingExtracted_${newFolderName}")
+			ssh(host, "sudo chmod 755 /etc/opt/it21_ui_${target}/gettingExtracted_${newFolderName}")
 			put(host, "./download/${zipDist}", "/etc/opt/it21_ui_${target}/gettingExtracted_${newFolderName}/${zipDist}")
 			ssh(host, "sudo unzip /etc/opt/it21_ui_${target}/gettingExtracted_${newFolderName}/${zipDist} -d /etc/opt/it21_ui_${target}/gettingExtracted_${newFolderName}")
 			put(host, "/etc/opt/apgops/config/${target}/it21-gui/AdGIS.properties", "/etc/opt/it21_ui_${target}/gettingExtracted_${newFolderName}/conf/AdGIS.properties")
