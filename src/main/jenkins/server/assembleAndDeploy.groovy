6be45ba37ae7e44bae91f0dd7ae581e566f77ef3
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-06-24 15:09:44 +0200

    how to use variable in declarative syntax

--- src/main/jenkins/server/assembleAndDeploy.groovy
+++ src/main/jenkins/server/assembleAndDeploy.groovy
@@ -7,14 +7,14 @@ pipeline {
         stage("Initializing") {
             steps {
                 echo "Pipeline is running for target ${TARGET}"
+                script {
+                    dirName = "20200624_12345"
+                }
+                echo "Name of directory containing JSON Patch file: ${env.dirName}"
             }
         }
+
         stage("Getting JSON Patch files") {
-
-            script {
-                dirName = "20200624_12345"
-            }
-
             steps {
                 echo "JSON Patch files will be taken from here"
                 sh("mkdir ${env.dirName}")
