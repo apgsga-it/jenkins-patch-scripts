610059049594e2e8107dec2ec28190bb0a0a2e56
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-06-24 12:46:09 +0200

    need steps block

--- src/main/jenkins/server/assembleAndDeploy.groovy
+++ src/main/jenkins/server/assembleAndDeploy.groovy
@@ -1,12 +1,16 @@
 pipeline {
-
+    agent any
     stages {
         stage("Initializing") {
-            echo "Pipeline is running for target ${TARGET}"
+            steps {
+                echo "Pipeline is running for target ${TARGET}"
+            }
         }
         stage("Getting JSON Patch files") {
-            echo "JSON Patch files will be taken from here"
-            sh("ls -la")
+            steps {
+                echo "JSON Patch files will be taken from here"
+                sh("ls -la")
+            }
         }
     }
 
