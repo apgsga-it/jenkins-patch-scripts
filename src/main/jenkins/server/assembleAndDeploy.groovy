61ce34c8217fad8d0af7afbba4b85f7dd2864be8
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-06-24 12:42:17 +0200

    reading parameter and calling a sample sh function

--- src/main/jenkins/server/assembleAndDeploy.groovy
+++ src/main/jenkins/server/assembleAndDeploy.groovy
@@ -1,3 +1,13 @@
-node {
-	echo "This is a test"
+pipeline {
+
+    stages {
+        stage("Initializing") {
+            echo "Pipeline is running for target ${TARGET}"
+        }
+        stage("Getting JSON Patch files") {
+            echo "JSON Patch files will be taken from here"
+            sh("ls -la")
+        }
+    }
+
 }
