227fae9efd4f988513722715b95e08c55d40ba79
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-07-01 09:30:37 +0200

    missing some parameters to deploy

--- vars/patchADFunctions.groovy
+++ vars/patchADFunctions.groovy
@@ -46,13 +46,13 @@ def assemble() {
 def deploy() {
 	// TODO JHE: Obvisously things to be adapted, basically all parameter which will come from patchConfig, I guess
 	dir("digiflex-jadas-pkg") {
-		sh "./gradlew clean deployRpm -PtargetHost=dev-jhedocker.light.apgsga.ch -Dgradle.user.home=/var/jenkins/gradle/plugindevl --info --stacktrace"
+		sh "./gradlew clean deployRpm -PtargetHost=dev-jhedocker.light.apgsga.ch -PbaseVersion=1.0 -PrpmReleaseNr=222 -Dgradle.user.home=/var/jenkins/gradle/plugindevl --info --stacktrace"
 	}
 	dir("digiflex-it21-ui-pkg") {
-		sh "./gradlew clean deployZip -PtargetHost=dev-jhedocker.light.apgsga.ch -Dgradle.user.home=/var/jenkins/gradle/plugindevl --info --stacktrace"
+		sh "./gradlew clean deployZip -PtargetHost=dev-jhedocker.light.apgsga.ch -PbaseVersion=1.0 -PrpmReleaseNr=222 -Dgradle.user.home=/var/jenkins/gradle/plugindevl --info --stacktrace"
 	}
 	dir("digiflex-web-it21-pkg") {
-		sh "./gradlew clean deployRpm -PtargetHost=dev-jhedocker.light.apgsga.ch -Dgradle.user.home=/var/jenkins/gradle/plugindevl --info --stacktrace"
+		sh "./gradlew clean deployRpm -PtargetHost=dev-jhedocker.light.apgsga.ch -PbaseVersion=1.0 -PrpmReleaseNr=222 -Dgradle.user.home=/var/jenkins/gradle/plugindevl --info --stacktrace"
 	}
 }
 
