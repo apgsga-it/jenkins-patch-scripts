ce96765b8332bcbe05db37e3c3ecc2d50ffd95b7
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-07-22 15:46:11 +0200

    need to unstash

--- src/main/jenkins/server/assembleAndDeploy.groovy
+++ src/main/jenkins/server/assembleAndDeploy.groovy
@@ -2,12 +2,10 @@ library("patch-ad-functions-library")
 def targetSystemMappingFile = libraryResource("TargetSystemMappings.json")
 // TODO JHE: To be verified with UGE, which code should we use, new ones? 113 is definitely wrong, just as an example. Also, CHEI212 probably won't be in the list here
 def targetCodeStatus = ["chei212":"113","chei211":"xxx","chti211":"yyy","chpi211":"zzz"]
-def dirName = new Date().format("yyyyMMdd_HHmmssSSS")
 def serviceInPatches = ""
-def stashName = "${dirName}_stashed"
+def stashName = new Date().format("yyyyMMdd_HHmmssSSS")
 
 echo "Pipeline is running for target ${TARGET}"
-echo "Temporary path of directory containing JSON Patch files which will be stashed: ${env.WORKSPACE}/${dirName}"
 echo "Stash name : ${stashName}"
 
 pipeline {
@@ -17,9 +15,9 @@ pipeline {
             steps {
                 sh("mkdir ${dirName}")
                 // TODO JHE: That should work without specifying the full path, but seems that Jenkins declarative pipeline is using a non-shell script, meaning /etc/profile.d or .bashrc files are not getting interpreted
-                sh("/opt/apg-patch-cli/bin/apscli.sh -cpf ${targetCodeStatus.get(TARGET)},${dirName}")
-                stash name: stashName, includes: "${dirName}/*"
-                sh("rm -rf ${dirName}")
+                sh("/opt/apg-patch-cli/bin/apscli.sh -cpf ${targetCodeStatus.get(TARGET)},${env.WORKSPACE}")
+                stash name: stashName, includes: "*.json"
+                sh("rm -f *.json")
 
             }
         }
@@ -43,7 +41,7 @@ pipeline {
         stage("Assembling and deploying projects") {
             steps {
                 script {
-                        patchADFunctions.assembleAndDeploy(TARGET,"${env.WORKSPACE}/${dirName}",targetSystemMappingFile,serviceInPatches)
+                        patchADFunctions.assembleAndDeploy(TARGET,stashName,targetSystemMappingFile,serviceInPatches)
                     }
             }
         }
