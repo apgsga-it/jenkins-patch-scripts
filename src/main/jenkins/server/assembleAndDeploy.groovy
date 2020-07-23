61bd702fc70a29eba49804356290fbbf0dcef1f3
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-07-23 08:37:43 +0200

    .

--- src/main/jenkins/server/assembleAndDeploy.groovy
+++ src/main/jenkins/server/assembleAndDeploy.groovy
@@ -44,5 +44,10 @@ pipeline {
                     }
             }
         }
+
+        stage("Cleaning Workspace") {
+            // JHE: The stash can be preserved, but we don't have to keep the folder where stashed files have been extracted
+            sh("rm -rf ${stashName}")
+        }
     }
 }
