f82f06e66ee8a31184d6c0e5e3c225f206e8cf6d
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-10-09 13:29:57 +0200

    build/assemble/publish/install only when needed

--- vars/patchDeployment.groovy
+++ vars/patchDeployment.groovy
@@ -19,7 +19,9 @@ def installDeploymentArtifacts(patchConfig) {
 			// JHE (29.10.2019): DB part is not yet ready to be installed with SSH
 			// installerFactory('it21-db', patchConfig.currentTarget).call()
 			node {
-				installDbPatch(patchConfig,patchfunctions.getCoPatchDbFolderName(patchConfig),"zip",getHost("it21-db",patchConfig.currentTarget),getType("it21-db",patchConfig.currentTarget))
+				if(patchConfig.installDockerServices || !patchConfig.dbObjects.empty) {
+					installDbPatch(patchConfig, patchfunctions.getCoPatchDbFolderName(patchConfig), "zip", getHost("it21-db", patchConfig.currentTarget), getType("it21-db", patchConfig.currentTarget))
+				}
 			}
 		}
 	}
